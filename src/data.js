0875233cb2e62d7b9c37c5ec2b350919398f404b
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-06-10 14:40:55 -0400

    Add many funcitions

---
+++ src/data.js
@@ -0,0 +1,75 @@
+import img1 from './Project-01.gif'
+import img2 from './Project-02.gif'
+import img3 from './Project-03.gif'
+import img4 from './Project-04.gif'
+
+export const projects = [
+  {
+    title: "Dream Team",
+    subtitle: "Boostrap & Web Services API",
+    description:
+      "Project developed for basketball fans who like to keep track of their favorite player's numbers. App developed with a team of 4 people aiming to put into practice concepts learned during classes.",
+    image: img1,
+    link: "https://github.com/RodrigoBS1/Dream-Team",
+  },
+  {
+    title: "JarkGPT",
+    subtitle: "Boostrap and Sequelize",
+    description:
+      "Project with 3 other classmates in order to put learning into practice using database manipulation tools. The tool created has the objective of teaching the AI user to better use the tool when asking questions",
+    image: img2,
+    link: "https://jarkfront-end1.onrender.com/",
+  },
+  {
+    title: "Age Function",
+    subtitle: "JavaScript & DOM",
+    description:
+      "Simple tool created to verify age. The intention was to practice DOM, functions in JavaScript, HTML and CSS. The layout is simple and emphasizing only the age selector.",
+    image: img3,
+    link: "https://github.com/RodrigoBS1",
+  },
+  {
+    title: "Coffee",
+    subtitle: "DOM & Web Services API",
+    description:
+      "Coffee picker using external data source. Putting into practice concepts learned in class built with HTML, CSS, Bootstrap, JavaScript, Web Services API, JSON.",
+    image: img4,
+    link: "https://github.com/RodrigoBS1",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Rodrigo is a very diligent and very resilient guy, I am proud to have known and witnessed his attitudes and how diligent and responsible he is. I strongly recommend him.",
+    image:
+      "https://media.licdn.com/dms/image/C4D03AQGWFylapckjBw/profile-displayphoto-shrink_800_800/0/1564761329781?e=1689811200&v=beta&t=ObcPinF3wjaKcsKRimqGRLYlhuOCHDcyxWnl23kBseU",
+    name: "Gabriel Tinti",
+    company: "Capgemini",
+  },
+  {
+    quote:
+      "I had the pleasure of meeting Rodrigo and mentoring him so I could see how exceptional he is, helpful, courageous and always ready to solve problems and learn new things.",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQFWv8yXllTUPQ/profile-displayphoto-shrink_800_800/0/1678811464424?e=1689811200&v=beta&t=UMvCwdjFuxoQWtKoZjnY--vdHNr2WVfIGEXk_ND1JOM",
+    name: "Marcelo Pereira",
+    company: "Improving",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Express.js",
+  "Node.js",
+  "PostgreSQL",
+  "Bootstrap",
+  "Sequelize",
+  "Git & GitHub",
+];
+
+export const resume = [{
+  resume: "/public/RodrigoSilva-2023-Resume.pdf"
+}]
